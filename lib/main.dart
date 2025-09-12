a3572a0e9da87b7744169430e74d1457edea2bdd
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-09-12 10:39:23 +0700

    Add multiple child demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week05/single_child_demo.dart';
+import 'package:flutter_application/week05/multiple_child_demo2.dart';
 
-void main() => runApp(MaterialApp(home: SingleChildDemo()));
+void main() => runApp(MaterialApp(home: MultipleChildDemo2()));
