ca7a22caa1c835549467f341559ba1f3a0a3a2b7
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-09-12 10:51:10 +0700

    Add the simple row column design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week05/multiple_child_demo2.dart';
+import 'package:flutter_application/week05/design1.dart';
 
-void main() => runApp(MaterialApp(home: MultipleChildDemo2()));
+void main() => runApp(MaterialApp(home: Design1()));
