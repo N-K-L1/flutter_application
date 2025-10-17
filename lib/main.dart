55faa0f8aac0ed79acea9f0732cd6c7b91832992
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-10-17 09:39:47 +0700

    Done scroll_view_demo.dart as exercise in class

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/week08/scroll_view_demo.dart';
 
 void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "My first app",
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: const Center(child: Text("Hello world")),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        backgroundColor: Colors.orange,
-        child: Text("click"),
-      ),
-    ),
-  ),
+  MaterialApp(debugShowCheckedModeBanner: false, home: ScrollviewDemo()),
 );
---
+++ lib/week08/tab_demo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabDemo extends StatelessWidget {
+  const TabDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
