126f60e9d17e2ad2fc0cf9c567e2355248a27f5c
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-10-17 10:16:00 +0700

    Done pageview_demo.dart as in class exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week08/tab_demo.dart';
+import 'package:flutter_application/week08/pageview_demo.dart';
 
-void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TabDemo()));
+void main() => runApp(
+  MaterialApp(debugShowCheckedModeBanner: false, home: PageviewDemo()),
+);
