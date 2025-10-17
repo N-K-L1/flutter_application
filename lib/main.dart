5f29785add6808567e7263a61728e90c7057cf5a
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-10-17 10:43:49 +0700

    Done page1.dart and page2.dart as in class exercise that does transfer info from one dart file to another dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week08/pageview_demo.dart';
+import 'package:flutter_application/week08/page1.dart';
 
-void main() => runApp(
-  MaterialApp(debugShowCheckedModeBanner: false, home: PageviewDemo()),
-);
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Page1()));
