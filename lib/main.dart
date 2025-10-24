1c6b66e44b9d3905efa5482f6c12b253813abff8
N-K-L1 <6631501179@lamduan.mfu.ac.th>
2025-10-24 10:01:53 +0700

    Done drawer_demo.dart as in-class exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week09/snack_bar_demo.dart';
+import 'package:flutter_application/week09/drawer_demo.dart';
 
-void main() => runApp(
-  MaterialApp(debugShowCheckedModeBanner: false, home: SnackBarDemo()),
-);
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: DrawerDemo()));
---
+++ lib/week09/drawer_demo.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  const DrawerDemo({super.key});
+  // Method to create a drawer
+  Widget createDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(color: Colors.lightBlue),
+            child: Row(
+              children: [
+                //Image.asset('assets/images/batman.png', width: 100),
+                Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeGVUGslmHUWyeqopY1PThHAnuQ8XL0E2RYw&s',
+                  width: 100,
+                ),
+                const Text('Header'),
+              ],
+            ),
+          ),
+          TextButton.icon(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.remove_circle),
+            label: const Text('Item 1'),
+          ),
+          TextButton.icon(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.add_circle),
+            label: const Text('Item 2'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Drawer Demo')),
+      drawer: createDrawer(context),
+      body: const Center(child: Text('Welcome page')),
+      // Disable opening the drawer with a swipe gesture.
+      drawerEnableOpenDragGesture: false, //default is true
+    );
+  }
+}
